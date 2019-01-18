2b104a748aeecf49027403d2464c6d0c9583fbca
Aceecapellan2000 <45424791+Aceecapellan2000@users.noreply.github.com>
2019-01-18 20:45:32 +0800

    Update Membershipsystem.py

--- Membershipsystem.py
+++ Membershipsystem.py
@@ -24,5 +24,26 @@ def voucher():
     return render_template('index4.html')
 
 
+@app.route('/aches')
+def aches():
+    return render_template('index6.html')
+
+
+@app.route('/article')
+def article():
+    return render_template('index5.html')
+
+
+@app.route('/diabetes')
+def diabetes():
+    return render_template('index7.html')
+
+
+@app.route('/stigma')
+def stigma():
+    return render_template('index8.html')
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
